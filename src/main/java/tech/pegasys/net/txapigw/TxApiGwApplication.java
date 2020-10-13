c73488cdd05e277566ee85a42f5717ef435940d3
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-10-13 18:48:21 +0200

    First commit

---
+++ src/main/java/tech/pegasys/net/txapigw/TxApiGwApplication.java
@@ -0,0 +1,13 @@
+package tech.pegasys.net.txapigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxApiGwApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(TxApiGwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/pegasys/net/txapigw/TxApiGwApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.pegasys.net.txapigw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxApiGwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
