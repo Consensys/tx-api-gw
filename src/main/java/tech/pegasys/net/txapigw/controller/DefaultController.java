c518ef979c02f25062e773e3a00cf955b840ffe5
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-10-21 17:54:27 +0200

    Update DefaultController.java

--- src/main/java/tech/pegasys/net/txapigw/controller/DefaultController.java
+++ src/main/java/tech/pegasys/net/txapigw/controller/DefaultController.java
@@ -4,15 +4,14 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 
 import io.swagger.v3.oas.annotations.Hidden;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Hidden
 @RestController
 public class DefaultController {
 
-  @RequestMapping(path = "/", method = RequestMethod.GET)
+  @GetMapping("/")
   public void redirect(HttpServletResponse response) throws IOException {
     response.sendRedirect("/swagger-ui.html");
   }
