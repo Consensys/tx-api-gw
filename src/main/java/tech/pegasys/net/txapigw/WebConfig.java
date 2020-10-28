821f15a316b0d6f3fb067c843247ce08153aeb70
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-10-28 09:54:06 +0100

    add log filter to display request info in logs

--- src/main/java/tech/pegasys/net/txapigw/WebConfig.java
+++ src/main/java/tech/pegasys/net/txapigw/WebConfig.java
@@ -1,6 +1,8 @@
 package tech.pegasys.net.txapigw;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,7 +10,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
   @Override
-  public void addCorsMappings(CorsRegistry registry) {
+  public void addCorsMappings(final CorsRegistry registry) {
     registry.addMapping("/**");
+  }
+
+  @Bean
+  public CommonsRequestLoggingFilter logFilter() {
+    final CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+    filter.setIncludeQueryString(true);
+    filter.setIncludePayload(true);
+    filter.setMaxPayloadLength(10000);
+    filter.setIncludeHeaders(false);
+    filter.setAfterMessagePrefix("REQUEST DATA : ");
+    return filter;
   }
 }
